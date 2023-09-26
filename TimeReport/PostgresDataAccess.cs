b6913451f33b6a8d410d23bd70acd1d55123ca50
Toxicwhisper <morgan.lindzen@gmail.com>
2023-09-26 12:19:49 +0200

    restructured program, added UniqueIDs for people and projects, added recording of hours worked, and also error message when program can not find person or project.

--- TimeReport/PostgresDataAccess.cs
+++ TimeReport/PostgresDataAccess.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using Npgsql;
 using Dapper;
@@ -21,12 +22,12 @@ public class PostgresDataAccess
         }
     }
 
-    public void InsertProject(string projectName)
+    public void InsertProject(string projectName, int projectId)
     {
         using (IDbConnection dbConnection = new NpgsqlConnection(_connectionString))
         {
             dbConnection.Open();
-            dbConnection.Execute("INSERT INTO mjl_project (project_name) VALUES (@ProjectName)", new { ProjectName = projectName });
+            dbConnection.Execute("INSERT INTO mjl_project (id, project_name) VALUES (@ProjectId, @ProjectName)", new { ProjectId = projectId, ProjectName = projectName });
         }
     }
 
@@ -37,6 +38,25 @@ public class PostgresDataAccess
             dbConnection.Open();
             dbConnection.Execute("INSERT INTO mjl_project_person (project_id, person_id, hours) VALUES (@ProjectId, @PersonId, @HoursWorked)",
                 new { ProjectId = projectId, PersonId = personId, HoursWorked = hoursWorked });
+        }
+    }
+
+    //methods added to be able to retrieve project ID and Person ID by name. 
+    public int GetPersonIdByName(string personName)
+    {
+        using (IDbConnection dbConnection = new NpgsqlConnection(_connectionString))
+        {
+            dbConnection.Open();
+            return dbConnection.QueryFirstOrDefault<int>("SELECT id FROM mjl_person WHERE person_name = @PersonName", new { PersonName = personName });
+        }
+    }
+
+    public int GetProjectIdByName(string projectName)
+    {
+        using (IDbConnection dbConnection = new NpgsqlConnection(_connectionString))
+        {
+            dbConnection.Open();
+            return dbConnection.QueryFirstOrDefault<int>("SELECT id FROM mjl_project WHERE project_name = @ProjectName", new { ProjectName = projectName });
         }
     }
 }
--- TimeReport/Program.cs
+++ TimeReport/Program.cs
@@ -1,21 +1,23 @@
 ﻿using System;
-
-using System;
-
-using Npgsql;
+using System.Collections.Generic;
 
 class Program
 {
+    static int personIdCounter = 1;
+    static int projectIdCounter = 1;
+    static Dictionary<string, int> personIdMap = new Dictionary<string, int>();
+    static Dictionary<string, int> projectIdMap = new Dictionary<string, int>();
+
     static void Main(string[] args)
     {
         string connectionString = "Server=dev.kjeldcon.se;Port=5432;Userid=monsters;Password=monsters123;Pooling=false;MinPoolSize=1;MaxPoolSize=20;Timeout=15;SslMode=Disable;Database=monsters";
 
         PostgresDataAccess dataAccess = new PostgresDataAccess(connectionString);
 
-       
-    
+        string personName; 
+        string projectName;
 
-while (true)
+        while (true)
         {
             Console.WriteLine("Time Tracking System Menu:");
             Console.WriteLine("1. Skapa Person: ");
@@ -30,29 +32,51 @@ while (true)
             {
                 case "1":
                     Console.Write("Ange person: ");
-                    string personName = Console.ReadLine();
-                    dataAccess.InsertPerson(personName);
+                    personName = Console.ReadLine();
+                    dataAccess.InsertPerson(personName); //Generate personID
+                    personIdMap[personName] = personIdCounter; //connects generated id to persons
+                    personIdCounter++; // introduced counter to generate unique IDs for persons
                     Console.WriteLine("Person skapad.");
                     break;
                 case "2":
                     Console.Write("Ange projekt: ");
-                    string projectName = Console.ReadLine();
-                    dataAccess.InsertProject(projectName);
+                    projectName = Console.ReadLine();
+                    Console.WriteLine($"Before increment: projectIdCounter = {projectIdCounter}");
+                    dataAccess.InsertProject(projectName, projectIdCounter); //Generates project ID
+                    projectIdMap[projectName] = projectIdCounter; //Connects project ID to name
+                    projectIdCounter++; //makes sure that the ID is unique
+                    Console.WriteLine($"After increment: projectIdCounter = {projectIdCounter}");
                     Console.WriteLine("Projekt skapat");
                     break;
-                case "3":
-                    Console.Write("Ange PersonID: ");
-                    int personId = int.Parse(Console.ReadLine());
 
-                    Console.Write("Ange ProjektID: ");
-                    int projectId = int.Parse(Console.ReadLine());
+
+
+                case "3":
+                    Console.Write("Ange personens namn: ");
+                    personName = Console.ReadLine();
+
+                    Console.Write("Ange projektets namn: ");
+                    projectName = Console.ReadLine();
 
                     Console.Write("Ange arbetade timmar: ");
                     int hoursWorked = int.Parse(Console.ReadLine());
 
-                    dataAccess.RecordWorkedHours(personId, projectId, hoursWorked);
-                    Console.WriteLine("Timmar loggade. ");
+                    // Look up person and project IDs based on their names
+                    int personId = dataAccess.GetPersonIdByName(personName);
+                    int projectId = dataAccess.GetProjectIdByName(projectName);
+
+                    // checks if valid persons
+                    if (personId == -1 || projectId == -1)
+                    {
+                        Console.WriteLine("Personen eller projektet hittades inte.");
+                    }
+                    else
+                    {
+                        dataAccess.RecordWorkedHours(personId, projectId, hoursWorked);
+                        Console.WriteLine("Timmar loggade.");
+                    }
                     break;
+
                 case "4":
                     Environment.Exit(0);
                     break;
